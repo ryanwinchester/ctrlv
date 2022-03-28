7dde6384550c581ac7c7c9b15bac0af324ca20c6
Ryan Winchester <ryan@sevenshores.co>
2022-03-28 09:37:40 -0300

    monaco-editor

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,5 +1,23 @@
 import * as Monaco from 'monaco-editor/esm/vs/editor/editor.main';
 
+self.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId, label) {
+    if (label === 'json') {
+      return './vs/language/json/json.worker.js';
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return './vs/language/css/css.worker.js';
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return './vs/language/html/html.worker.js';
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return './vs/language/typescript/ts.worker.js';
+    }
+    return './vs/editor/editor.worker.js';
+  }
+};
+
 export default function createEditor(el) {
   Monaco.editor.create(el, {
     value: ['function x() {', '    console.log("Hello world!");', '}'].join('\n'),
